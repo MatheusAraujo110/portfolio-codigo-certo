c5df3c74ed431dacb7701aad3d89ea8c3d9f9408
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-08-27 23:18:59 -0300

    alterando navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,70 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import Logo from "../../assets/Logo (2).jpeg"
 
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu)
-        this.navList = document.querySelector(navList)
-        this.navLinks = document.querySelectorAll(navLinks)
-        this.activeClass = "active"
-
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
-        })
-    }
-
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass)
-        this.mobileMenu.classList.toggle(this.activeClass)
-        this.animateLinks()
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick)
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent()
-        }
-        return this
-    }
-}
-
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-)
-mobileNavbar.init()
-
 function NavBar() {
+    const [active, setActive] = useState("nav-menu")
+    const [toggleIcon, setToggleIcon] = useState("nav-toggler")
+
+    const navToggle = () => {
+        active === "nav-menu" ? setActive("nav-menu nav-active") : setActive("nav-menu")
+        // TogglerIcon
+        toggleIcon === "nav-toggler" ? setToggleIcon("nav-toggler toggle") : setToggleIcon("nav-toggler")
+    }
 
     return (
         <header className='header'>
             <img src={Logo} alt="logo" className='logo' />
 
-            <nav className='navbar'>
-                <div className='mobile-menu'>
+            <nav className='nav'>
+                <ul className={active}>
+                    <a href="#" className="nav-link">Home</a>
+                    <li className="nav-item"><a href="#aboutme" className="nav-link">Sobre mim</a></li>
+                    <li className="nav-item"><a href="#personal-tastes" className="nav-link">Gostos Pessoais</a></li>
+                    <li className="nav-item"><a href="#motivations" className="nav-link">Motivações</a></li>
+                </ul>
+
+                <div onClick={navToggle} className={toggleIcon}>
                     <div className='line1'></div>
                     <div className='line2'></div>
                     <div className='line3'></div>
                 </div>
-                <ul className='nav-list'>
-                    <a href='#'>Home</a>
-                    <li><a href='#aboutme'>Sobre mim</a></li>
-                    <li><a href='#personal-tastes'>Gostos Pessoais</a></li>
-                    <li><a href='#motivations'>Motivações</a></li>
-                </ul>
             </nav>
-        </header >
+        </header>
     )
 }
 
